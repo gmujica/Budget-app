a4b99e461f751362928058eca88fc4809189e267
gmujica <gregorymujica1993@gmail.com>
2018-12-25 15:52:23 -0400

    estructura basica

---
+++ app.js
@@ -0,0 +1,32 @@
+// BUDGET CONTROLLER
+var budgetController = (function() {
+
+    // Some code
+
+})();
+
+// UI CONTROLLER
+var UIController = (function() {
+
+    // Some code
+
+})();
+
+// GLOBAL APP CONTROLLER
+var controller = (function(budgetCtrl, UICtrl) {
+
+        document.querySelector('.add__btn').addEventListener('click', function() {
+            
+            // 1. Get the field input data
+
+            //2. Add the item to the budget controller
+
+            // 3. Add the item to the UI
+
+            // 4. Calculate the butget
+
+            // 5. Display the butget on the UI
+            
+        });
+
+})(budgetController, UIController);
